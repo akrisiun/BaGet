eafd07188befd52532a635ce7a08938877498f91
Andrius-K <akrisiun@gmail.com>
2021-01-29 08:53:50 +0200

    UseEndpoints TODO, build fixes

--- build.ps1
+++ build.ps1
@@ -1,15 +1,19 @@
 #!/usr/local/bin/pwsh
 
+if ($PsVersionTable.Platform -eq "Unix") {
+   mkdir app/
+} else {
 mkdir -f app/
+}
 
 dotnet restore
 
 $p = $PWD
-cd src\BaGet.UI\
+Set-Location src\BaGet.UI\
 yarn 
 yarn build
 
-cd $p
+Set-Location $p
 
 dotnet build   src/Baget
 dotnet publish src/Baget -o $PWD/app
